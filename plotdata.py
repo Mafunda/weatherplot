daa46257b49149f50ffb60a00d0a6e91a019ffc0
Martin Mafunda <martinmafunda@gmail.com>
2018-01-20 11:21:59 +0200

    add a hello

--- plotdata.py
+++ plotdata.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as pl
 data1 = np.loadtxt("testdata.csv", delimiter=",", skiprows=1)
 #this is a comment
+print("Hello!")
 pl.figure(1)
 pl.plot(data1[:,0],data1[:,1], "r--*")
 pl.xlabel("time:")
