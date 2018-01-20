572da2d1186ffab6aa2e0a974c19a38095c8565c
Martin Mafunda <martinmafunda@gmail.com>
2018-01-20 10:08:33 +0200

    add script to plot weather data

---
+++ plotdata.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as pl
+data1 = np.loadtxt("testdata.csv", delimiter=",", skiprows=1)
+pl.figure(1)
+pl.plot(data1[:,0],data1[:,1], "r--*")
+pl.xlabel("time:")
+pl.ylabel("${}^0C$")
+pl.legend(["temp"])
+xx = np.arange(0,100,0.5)
+yy= np.sin(xx/100*np.pi)*20+10
+pl.figure(2)
+pl.plot(xx,yy,"g")
+pl.legend(["wave"])
+pl.xlim(xmax=150,xmin=0)
+pl.title("My graph")
+pl.show()
